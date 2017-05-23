4b417cb732356baba15a548c15bd7636566bcc77
luislocon <luis.locon@gmail.com>
2017-05-23 16:56:53 -0600

    Agregando un ejemplo de funciones para calcular areas

---
+++ areas.js
@@ -0,0 +1,13 @@
+//Area de un triangulo
+let base = 5;
+let altura = 7;
+//Usando arrow functions
+const triangleArea = (base, altura)=> base*altura/2
+console.log(`El area de un triangulo de base ${base} y altura ${altura} es: ${triangleArea(base, altura)}`);
+
+
+//Area de un circulo
+let r = 4;
+//Arrow function
+const circleArea = (radio) => Math.PI * Math.pow(r, 2);
+console.log(`El area de un circulo de radio ${r} es: ${circleArea()}`)
