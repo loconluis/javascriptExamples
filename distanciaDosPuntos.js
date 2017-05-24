3e9ee73b54a1845eec2161fa7f00561760ddc95a
luislocon <luis.locon@gmail.com>
2017-05-23 22:24:09 -0600

    Objetos con Javascript con keyword class

--- distanciaDosPuntos.js
+++ distanciaDosPuntos.js
@@ -1,47 +1,26 @@
-//Objetos en javascript
-//Forma de crear objetos con una funcion
-// function Punto(x, y){
-//   this.x = x;
-//   this.y = y;
-// }
-// Punto.prototype.moverX = function moverX(x){
-//   this.x+=x;
-// }
-// Punto.prototype.moverY = function moverY(y){
-//   this.y+=y;
-// }
-//
-// Punto.prototype.distancia = function distancia(p){
-//   const x = this.x - p.x;
-//   const y = this.y - p.y;
-//
-//   return Math.sqrt(x*x + y*y)
-// }
-
-const Punto = {
-  init: function init(x, y){
-    this.x = x;
+class Punto{
+  constructor(x, y){
+    this.x =x;
     this.y = y;
-  },
-  moverX: function moverX(x){
+  }
+  moverX(x){
     this.x+=x;
-  },
-  moverY: function moverY(y){
+  }
+  moverY(y){
     this.y+=y;
-  },
-  distancia: function distancia(p){
+  }
+  distancia(p){
     const x = this.x - p.x;
     const y = this.y - p.y;
-
-    return Math.sqrt(x*x+y*y);
+    return Math.sqrt(x*x + y*y)
   }
 }
 
-const p1 = Object.create(Punto);
-const p2 = Object.create(Punto);
+const p1 = new Punto(0, 4);
+const p2 = new Punto(3, 0);
 
-p1.init(0,4);
-p2.init(3,0)
+//p1.init(0,4);
+//p2.init(3,0)
 
 // const p1 = new Punto(0,4);
 // const p2 = new Punto(3,0);
@@ -66,3 +45,45 @@ const p2 = {
   moverX: function(x){this.x+= x},
   moverY: function(y){this.y+=y}
 };*/
+
+//Objetos en javascript
+//Forma de crear objetos con una funcion
+// function Punto(x, y){
+//   this.x = x;
+//   this.y = y;
+// }
+// Punto.prototype.moverX = function moverX(x){
+//   this.x+=x;
+// }
+// Punto.prototype.moverY = function moverY(y){
+//   this.y+=y;
+// }
+//
+// Punto.prototype.distancia = function distancia(p){
+//   const x = this.x - p.x;
+//   const y = this.y - p.y;
+//
+//   return Math.sqrt(x*x + y*y)
+// }
+
+// const Punto = {
+//   init: function init(x, y){
+//     this.x = x;
+//     this.y = y;
+//   },
+//   moverX: function moverX(x){
+//     this.x+=x;
+//   },
+//   moverY: function moverY(y){
+//     this.y+=y;
+//   },
+//   distancia: function distancia(p){
+//     const x = this.x - p.x;
+//     const y = this.y - p.y;
+//
+//     return Math.sqrt(x*x+y*y);
+//   }
+// }
+//
+// const p1 = Object.create(Punto);
+// const p2 = Object.create(Punto);
