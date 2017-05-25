7ca1b79d3963ea2deea51de5c758d6781702ccb6
luislocon <luis.locon@gmail.com>
2017-05-24 18:45:52 -0600

    Aprendiendo el scope de las variables

---
+++ scopeVariables.js
@@ -0,0 +1,6 @@
+
+var nombre = "Luis" //Esta siempre en el scope global
+let apellido = "Locon" //Let pertenece al scope en el cual se le esta declarando
+const edad = 23 //Const son variables que nunca cambian
+
+//Utiliza const y si no puedes let >>WARNING<< no uses VAR
